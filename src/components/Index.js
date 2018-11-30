935382205fcf58b938aebb8185598686c5a61646
Szymon Kluk <sizzar12@gmail.com>
2018-11-30 17:07:57 +0100

    Contact2

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,12 @@
 import Head from "./Head/Head";
 import React, { Component, Fragment } from "react";
-
+import About from "./About/About";
 export default class Index extends Component {
   render() {
     return (
       <Fragment>
         <Head />
+        <About />
       </Fragment>
     );
   }
