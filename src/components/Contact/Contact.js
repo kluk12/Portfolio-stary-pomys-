b5de50723cc80198e80303811f46114e5271dd18
Szymon Kluk <sizzar12@gmail.com>
2018-11-30 16:22:25 +0100

    Contact

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText
+} from "reactstrap";
+import "./Contact.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default class Example extends React.Component {
+  render() {
+    return (
+      <div className="contener--margiin">
+        <Form>
+          <FormGroup row>
+            <Label for="exampleEmail" sm={2}>
+              Email
+            </Label>
+            <Col sm={10}>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="with a placeholder"
+              />
+            </Col>
+          </FormGroup>
+
+          <Col sm={10} md={6}>
+            <FormGroup>
+              <Label for="exampleCity">City</Label>
+              <Input type="text" name="city" id="exampleCity" />
+            </FormGroup>
+          </Col>
+
+          <FormGroup row>
+            <Label for="exampleText" sm={2}>
+              Text Area
+            </Label>
+            <Col sm={10}>
+              <Input type="textarea" name="text" id="exampleText" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup check row>
+            <Col sm={{ size: 10, offset: 2 }}>
+              <Button>Submit</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
+    );
+  }
+}
--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,13 @@
 import Head from "./Head/Head";
 import React, { Component, Fragment } from "react";
+import Contact from "./Contact/Contact";
 
 export default class Index extends Component {
   render() {
     return (
       <Fragment>
         <Head />
+        {/* <Contact /> */}
       </Fragment>
     );
   }
