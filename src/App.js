a997f61dfedfd245fb1f1b4a6f0d938732b1e6b1
Szymon Kluk <sizzar12@gmail.com>
2018-12-06 20:15:07 +0100

    slider done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Index from "./components/Index";
-import Slider from "./components/Slider/Slider";
+import SimpleSlider from "./components/Slider/Slider";
 class App extends Component {
   render() {
     return (
@@ -10,7 +10,7 @@ class App extends Component {
         <Fragment>
           <Switch>
             <Route exact path="/" component={Index} />
-            <Route exact path="/a" component={Slider} />
+            <Route exact path="/a" component={SimpleSlider} />
             {/* <Route path="/product" component={Product_list} />
               <Route path="/test" component={rightpanel} /> */}
           </Switch>
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,28 +1,69 @@
 import React from "react";
-import SlideShow from "react-image-show";
+import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+import "./slick.css";
+import "./slick-theme.css";
 
-const urlArray = [
-  "https://camo.githubusercontent.com/f63754b8412368e820601967af6dea84312b925b/68747470733a2f2f7265616374747261696e696e672e636f6d2f72656163742d726f757465722f616e64726f69642d6368726f6d652d313434783134342e706e67",
-  "https://camo.githubusercontent.com/f63754b8412368e820601967af6dea84312b925b/68747470733a2f2f7265616374747261696e696e672e636f6d2f72656163742d726f757465722f616e64726f69642d6368726f6d652d313434783134342e706e67"
-];
-
-class Slider extends React.Component {
+class SimpleSlider extends React.Component {
   render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      centerMode: true,
+      focusOnSelect: true,
+      rows: 1,
+      slidesToShow: 4,
+      slidesToScroll: 2
+    };
     return (
-      <SlideShow
-        images={urlArray}
-        width="920px"
-        imagesWidth="800px"
-        imagesHeight="450px"
-        imagesHeightMobile="56vw"
-        thumbnailsWidth="920px"
-        thumbnailsHeight="12vw"
-        indicators
-        thumbnails
-        fixedImagesHeight
-        infinite={true}
-      />
+      <Slider {...settings}>
+        <div>
+          <img
+            src="http://webdevnatalia.com/img/logos/react.png"
+            alt="Tekst alternatywny"
+          />
+        </div>
+        <div>
+          <img
+            src="http://webdevnatalia.com/img/logos/html.png"
+            alt="Tekst alternatywny"
+          />
+        </div>
+        <div>
+          <img
+            src="http://webdevnatalia.com/img/logos/css.png"
+            alt="Tekst alternatywny"
+          />
+        </div>
+        <div>
+          <img
+            src="http://webdevnatalia.com/img/logos/bootstrap.png"
+            alt="Tekst alternatywny"
+          />
+        </div>
+        <div>
+          <img
+            src="http://webdevnatalia.com/img/logos/redux.png"
+            alt="Tekst alternatywny"
+          />
+        </div>
+        <div>
+          <img
+            src="http://webdevnatalia.com/img/logos/js-logo.png"
+            alt="Tekst alternatywny"
+          />
+        </div>
+        <div>
+          <img
+            src="http://webdevnatalia.com/img/logos/sass.png"
+            alt="Tekst alternatywny"
+          />
+        </div>
+      </Slider>
     );
   }
 }
-export default Slider;
+export default SimpleSlider;
